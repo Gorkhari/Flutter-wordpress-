04e395ba0cb052823a3670ce08a69675783cd5ee
gorkhari007 <51224729+gorkhari007@users.noreply.github.com>
2021-08-27 09:50:58 +1000

    Update config.dart

--- config.dart
+++ config.dart
@@ -1,6 +1,6 @@
 class Config {
-  static String key = "";
-  static String secret = "";
+  static String key = "YOUR_CONSUMER_KEY_FROM_REST_API";
+  static String secret = "YOUR_SECERT_KEY_FROM_REST_API";
   static String url = "https://example.com/wp-json/wc/v3/";
   static String customerURL = "customers";
   static String tokenURL = "https://example.com/wp-json/jwt-auth/v1/token";
